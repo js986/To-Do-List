585d644523570c9d9cfa73b83aa6dc666516c6d4
js986 <js986@njit.edu>
2019-08-18 12:52:47 -0400

    bug fixes

--- todo.js
+++ todo.js
@@ -3,25 +3,37 @@ var toDoList = {
 }
 
 function showList (){
-  var itemlist = document.getElementById('items');
-  itemlist.innerHTML = ""
- toDoList.items.forEach(function(element){
+  var items = document.getElementById('items');
+  items.innerHTML = ""
+  toDoList.items.forEach(function(element){
 
-   itemlist.innerHTML+=element;
-   itemlist.innerHTML+='<br>';
+   items.innerHTML+=element;
+   items.innerHTML+='<br>';
  })
+};
+
+
+function clearInput(text){
+  document.getElementById("inputtext").value = "";
 }
-var content = document.getElementById("additem")
-content.addEventListener("click",function(){
-  var item = document.createElement('input');
-  item.setAttribute('type','text');
+
+window.onload = function(){
+showList();
+//chrome.storage.local.set({"itemlist":toDoList.item},function(){
+  //console.log("Item is in local storage");
+//});
+document.getElementById("inputtext").onfocus = clearInput(this);
+document.getElementById("additem").onclick = function(){
+  var item = document.getElementById("inputtext").value;
+  //item.setAttribute('type','text');
   //item.setAttribute('name','text');
   //document.body.appendChild(item);
   //document.body.appendChild(document.createElement('br'));
-  if (item != null)
+  if (item != "")
     toDoList.items.push(item);
   showList();
-});
+}
+};
 /*function addToList(){
   //document.addEventListener('DOMContentLoaded',function(){
     var content = document.getElementById("additem");
